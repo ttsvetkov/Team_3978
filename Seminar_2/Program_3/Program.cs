8c846d0b46a8d3857d5bb829d9bed8dc09cad6ba
tsvetkov_av <t-tsvetkov@mail.ru>
2023-02-12 22:40:10 +0300

    Семинар 2 задача 3.1

--- Seminar_2/Program_3/Program.cs
+++ Seminar_2/Program_3/Program.cs
@@ -1,15 +1,16 @@
 ﻿Console.Clear();
 Console.WriteLine("Input number of day : ");
-
 int number = Convert.ToInt16(Console.ReadLine());
 
-if(number == 6 || number == 7) 
+if(number == 6 || number == 7) // number >= 6
 {
     Console.WriteLine("Выходной! Ура!!!");
 }
 else 
 {
-    Console.WriteLine("Рабочий день! пора на завод");        
+    Console.WriteLine("Рабочий день! Пора на завод!");        
 }
+
+
 
 
