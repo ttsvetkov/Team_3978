83d66b2811ec7a45cfc0e08009986afee7267389
tsvetkov_av <t-tsvetkov@mail.ru>
2023-02-12 22:02:04 +0300

    homework seminar_2 Задача_1

---
+++ Seminar_2/Program_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Введите трехзначное число :");
+
+int x = Convert.ToInt32(Console.ReadLine());
+int y = -1;
+
+if( x > 99 && x < 1000)
+{
+   y = (x / 10) % 10;
+   Console.WriteLine ($"Вторая цифра : {y}"); 
+}
+else Console.WriteLine("Error input");
