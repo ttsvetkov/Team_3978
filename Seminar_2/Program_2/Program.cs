80e2f5ffe5034ba558979e6525bdf3ab8e59cdd4
tsvetkov_av <t-tsvetkov@mail.ru>
2023-02-12 22:21:46 +0300

    Семинар 2 задача 2

---
+++ Seminar_2/Program_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Input number : ");
+var number = Console.ReadLine();
+analize(number);
+
+void analize(string s)
+{
+    if(s.Length < 3) 
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else 
+    {
+        Console.WriteLine($"Третья цифра : {s[2]}");        
+    }
+}
