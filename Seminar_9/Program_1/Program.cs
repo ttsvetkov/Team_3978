f1015b100c53f61c3ede8a09d3696d89cd154908
Tsvetkov Anatoly <t-tsvetkov@mail.ru>
2023-03-14 14:31:55 +0300

    homework 9

---
+++ Seminar_9/Program_1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+
+int number = Input.InputNumber("Введите число : ");
+
+///Метод вывода натуральных чисел от N до 1:
+void PrintNatural(int number)
+{
+    if (number < 0)
+    {
+        Console.Write($"{number} не натуральное число");
+        return;
+    }
+    else if (number == 0) return;
+    else
+    {
+        Console.Write($" {number},");
+        PrintNatural(number - 1);
+    }
+
+}
+
+PrintNatural(number);
---
+++ Seminar_9/Program_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+
+int numberM = Input.InputNumber("Введите начальное число M : ");
+int numberN = Input.InputNumber("Введите конечное число N : ");
+
+void Sum(int numberM, int numberN, int sum)
+{
+    if (numberM > numberN)
+    {
+        Console.WriteLine($"Сумма : {sum}");
+        return;
+    }
+    else
+    {
+        sum = sum + numberM;
+        Sum((numberM + 1), numberN, sum);
+    }
+}
+
+Sum(numberM, numberN, 0);
